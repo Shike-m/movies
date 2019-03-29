3701311a91a63edf0e8a73697340d723a9aa9107
shike.miao@gmail.com <shike.miao@gmail.com>
2019-03-28 19:25:00 -0500

    add back and forward button

--- src/components/movieList.js
+++ src/components/movieList.js
@@ -6,7 +6,9 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
-
+import Button from '@material-ui/core/Button';
+// import Icon from '@material-ui/core/Icon'
+// import Laptop from '@material-ui/icons/Laptop'
 
 const styles = theme => ({
     root: {
@@ -26,16 +28,48 @@ const styles = theme => ({
     titleBar: {
         background:
             'linear-grdient(to top,rgba(0,0,0,.7) 0%,rgba(0,0,0,.3) 100%)'
+    },
+    button: {
+        margin: theme.spacing.unit,
     }
 })
 
+const scrollLeft = () => {
+    var ele = document.getElementById('target');
+    ele.scrollBy({
+        left: -100,
+        behavior: 'smooth'
+    }) 
+}
+
+const scrollRight = () => {
+    var el = document.getElementById('target');
+    el.scrollBy({
+        left: 100,
+        behavior: 'smooth'
+    }) 
+}
+
 const MovieList = (props) => {
-    const { handleMovie,movies,classes } = props;
+    const { handleMovie, movies, classes } = props;
     return (
         <div className={classes.root}>
-            <GridList className={classes.gridList} cols={2.5}>
+            <Button className={classes.button} onClick={() => scrollLeft()}>
+                <i className="material-icons">
+                    arrow_back_ios
+                </i>
+            </Button>
+            <Button className={classes.button} onClick={() => scrollRight()}>
+                <i className="material-icons">
+                    arrow_forward_ios
+                </i>
+            </Button>
+            {/* <Icon className={classes.button}>arrow_back_ios</Icon>
+            <Icon className={classes.button}>arrow_forward_ios</Icon> */}
+            {/* <Laptop>Lenove</Laptop> */}
+            <GridList className={classes.gridList} cols={2.5} id='target'>
                 {movies.map(tile => (
-                    <GridListTile key={tile.id} onClick={()=>handleMovie(tile)}>
+                    <GridListTile key={tile.id} onClick={() => handleMovie(tile)}>
                         <img src={tile.images.medium} alt={tile.id} />
                         <GridListTileBar
                             title={tile.title}
@@ -58,7 +92,7 @@ const MovieList = (props) => {
 
 MovieList.propTypes = {
     classes: PropTypes.object.isRequired,
-  };
-  
+};
+
 // export default MovieList;
-  export default withStyles(styles)(MovieList);
+export default withStyles(styles)(MovieList);
